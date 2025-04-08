9ce8033bd484ac2b237a50e9e0a5c3a6e1a575d8
jpatino <julian.patino@sofwareone.com>
2025-04-08 17:58:57 -0500

    tercer commit 04-08-2025 - creacion de clase Condicionales

---
+++ src/com/empresa/CondicionalesIf.java
@@ -0,0 +1,48 @@
+package com.empresa;
+
+public class CondicionalesIf {
+
+	public static void main(String[] args) {
+		String var1 = "Hola";
+		String var2 = "Equipo";
+		
+		int a = 300;
+		int b = 200;
+		
+		//primer tipo de if
+		/*if(a>1000)
+		{
+			System.out.println("A es mayor que 1000");
+		}*/
+		
+		//Segundo tipo de if
+		/*if(a<1000)
+		{
+			System.out.println("A es menor que 1000");
+		}
+		else
+		{
+			System.out.println("A es mayor que 1000");
+		}*/
+
+		
+		if(a==100)
+		{
+			System.out.println("A es 100");
+		}
+		else if (a==200)
+		{
+			System.out.println("A es 200");
+		}
+		else if (a==300)
+		{
+			System.out.println("A es 300");
+		}
+		
+		else
+		{
+			System.out.println("A no es igual a ninguno de los numeros");
+		}
+	}
+
+}
