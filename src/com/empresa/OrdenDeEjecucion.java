fcc3dcb271daaef58139fc51b86780a5b73b9277
jpatino <julian.patino@sofwareone.com>
2025-04-08 17:35:10 -0500

    second commit 04-08-2025 - creacion de clase OrdenDeEjecucion

---
+++ src/com/empresa/OrdenDeEjecucion.java
@@ -0,0 +1,20 @@
+package com.empresa;
+
+public class OrdenDeEjecucion {
+
+	public static void main(String[] args) {
+		String var1 = "Hola";
+		String var2 = "Equipo";
+		
+		int a = 100;
+		int b = 200;
+		
+		System.out.println(var1+" "+var2);
+		System.out.println(a+b);
+		
+		System.out.println(a + b + var1 + var2);
+		System.out.println(var1 + var2 + a + b);
+
+	}
+
+}
