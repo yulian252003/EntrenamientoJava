29b3c338b2858801ca6c6190bfb96d945b9fe43c
jpatino <julian.patino@sofwareone.com>
2025-04-29 17:11:18 -0500

    Commit numero once-04/29/2025-Se explicó la palabra clave super

--- src/Opps/Carro.java
+++ src/Opps/Carro.java
@@ -1,6 +1,15 @@
 package Opps;
 
 public class Carro {
+	
+	String nombre="Segmento";
+	
+	Carro()
+	{
+		System.out.println("Creando el constructor de la clase carro");
+	}
+	
+	
 public void run()
 {
 	System.out.println("El carro está moviendose");
--- src/Opps/Mazda.java
+++ src/Opps/Mazda.java
@@ -1,13 +1,38 @@
 package Opps;
 
 public class Mazda extends Carro {
-	static String nombre="mazda";
+	//static String nombre="mazda";
+	String nombre="Mazda";
 	
+	public void getNombre() {
+		System.out.println(nombre);
+		System.out.println(super.nombre);
+	}
+	
+	public void run()
+	{
+		System.out.println("El Mazda está transitando");
+		super.run();
+	}
+	
+	Mazda()
+	{
+		//super();
+		System.out.println("Llamando al constructor de Mazda");
+		
+	}
 	
 	public static void main(String[]args) {
-		Carro car = new Carro();
-		System.out.println(nombre);
-		car.run();
+		//Carro car = new Carro();
+		//System.out.println(nombre);
+		//car.run();
+		
+		//Primer uso
+		Mazda maz= new Mazda();
+		maz.getNombre();
+		
+		//Segundo uso
+		maz.run();
 	}
 
 }
