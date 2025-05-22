4c26d0d9a01994a61286d010da826538f81d64b9
jpatino <julian.patino@sofwareone.com>
2025-05-22 16:51:59 -0500

    Commit numero 21-classes buffer and builder

--- src/strings/StringMethods.java
+++ src/strings/StringMethods.java
@@ -11,6 +11,14 @@ public class StringMethods {
 		System.out.println("Reemplazo: " + saludo.replace("Mundo", "Java"));
 		System.out.println(saludo.concat(" Bello"));
 		System.out.println("Termina en 'Hola'?" +saludo.endsWith("Hola"));
+		
+		System.out.println("-------------------------");
+		String text1="Hola Mundo";
+		System.out.println(text1.equals(saludo));
+		System.out.println("Indice de 'M': " + saludo.indexOf("Mundo"));
+		System.out.println("Indice de 'Mundo': " + saludo.indexOf("Mundo"));
+		System.out.println("Indice de 'Mundo': " + saludo.indexOf("Pepe"));
+		
 	}
 
 }
